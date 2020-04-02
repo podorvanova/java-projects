e20302d9576dd3bf3fc6fd84670a2c52a620dd2b
Maria Podorvanova <podorvanova.maria@gmail.com>
2020-04-02 14:12:20 +1100

    Implement sumDigits method

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -5,11 +5,18 @@ public class Main
         Container container = new Container();
         container.count += 7843;
 
+        Main newClass = new Main();
+        System.out.println(newClass.sumDigits(1010104));
+
     }
 
-    public Integer sumDigits(Integer number)
-    {
-        //@TODO: write code here
-        return 0;
+    public Integer sumDigits(Integer number) {
+        String numberString = number.toString();
+        int sumDigits = 0;
+        for (int i = 0; i < numberString.length(); i++) {
+            char symbol = numberString.charAt(i);
+            sumDigits += Integer.parseInt(String.valueOf(symbol));
+        }
+        return sumDigits;
     }
 }
