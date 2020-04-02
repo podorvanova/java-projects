73bfa2fa626dce457efda19b06e1cb75ec1d41be
Maria Podorvanova <podorvanova.maria@gmail.com>
2020-04-02 17:36:44 +1100

    Create MinAndMaxValues project

---
+++ 04_NumbersStringsAndDates/MinAndMaxValues/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        int minInt = Integer.MIN_VALUE;
+        int maxInt = Integer.MAX_VALUE;
+        System.out.println("Минимальное значение типа int " + minInt);
+        System.out.println("Максимальное значение типа int " + maxInt);
+
+        byte minByte = Byte.MIN_VALUE;
+        byte maxByte = Byte.MAX_VALUE;
+        System.out.println("Минимальное значение типа byte " + minByte);
+        System.out.println("Максимальное значение типа byte " + maxByte);
+
+        short minShort = Short.MIN_VALUE;
+        short maxShort = Short.MAX_VALUE;
+        System.out.println("Минимальное значение типа short " + minShort);
+        System.out.println("Максимальное значение типа short " + maxShort);
+
+        long minLong = Long.MIN_VALUE;
+        long maxLong = Long.MAX_VALUE;
+        System.out.println("Минимальное значение типа long " + minLong);
+        System.out.println("Максимальное значение типа long " + maxLong);
+
+        double minDouble = Double.MIN_VALUE;
+        double maxDouble = Double.MAX_VALUE;
+        System.out.println("Минимальное значение типа double " + minDouble);
+        System.out.println("Максимальное значение типа double " + maxDouble);
+
+        float minFloat = Float.MIN_VALUE;
+        float maxFloat = Float.MAX_VALUE;
+        System.out.println("Минимальное значение типа float " + minFloat);
+        System.out.println("Максимальное значение типа float " + maxFloat);
+    }
+}
