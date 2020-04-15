040d76a0d520ec7a6f6cd1dac31db7a60869b64b
Maria Podorvanova <podorvanova.maria@gmail.com>
2020-04-15 18:12:14 +1000

    Calculate total income

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -5,6 +5,16 @@ public class Loader
     {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
 
-        System.out.println(text);
+        //Зарплата Васи
+        int vasyaIncomeBeginIndex = text.indexOf('5');
+        String vasyaIncome = text.substring(vasyaIncomeBeginIndex, 19);
+
+        //Зарплата Маши
+        int mashaIncomeEndIndex = text.lastIndexOf('0');
+        String mashaPreIncome = text.substring(55, mashaIncomeEndIndex + 1);
+        String mashaIncome = mashaPreIncome.trim();
+
+        int sumIncome = Integer.parseInt(vasyaIncome) + Integer.parseInt(mashaIncome);
+        System.out.println("Суммарный доход Васи и Маши: " + sumIncome + " рублей");
     }
 }
