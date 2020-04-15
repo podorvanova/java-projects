4898f5e06e70c2055a4f1fd898f04fefdf97c57c
Maria Podorvanova <podorvanova.maria@gmail.com>
2020-04-15 19:40:08 +1000

    Parse full name

---
+++ 04_NumbersStringsAndDates/NameFormatting/src/Name.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Name {
+    public static void main(String[] args) throws IOException {
+        System.out.print("Введите ФИО: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String text = reader.readLine();
+
+        String familyName = text.substring(0, text.indexOf(' '));
+        String givenName = text.substring(text.indexOf(' ') + 1, text.lastIndexOf(' '));
+        String middleName = text.substring(text.lastIndexOf(' ') + 1);
+
+        System.out.println("Фамилия: " + familyName);
+        System.out.println("Имя: " + givenName);
+        System.out.println("Отчество: " + middleName);
+    }
+}
