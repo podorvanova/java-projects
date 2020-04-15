484d7c152e3049e8c593635f78abe15f3cf5a6a5
Maria Podorvanova <podorvanova.maria@gmail.com>
2020-04-15 19:58:15 +1000

    Check the length of a phrase

--- 04_NumbersStringsAndDates/NameFormatting/src/Name.java
+++ 04_NumbersStringsAndDates/NameFormatting/src/Name.java
@@ -4,16 +4,24 @@ import java.io.InputStreamReader;
 
 public class Name {
     public static void main(String[] args) throws IOException {
-        System.out.print("Введите ФИО: ");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String text = reader.readLine();
+        while(true) {
+            System.out.print("Введите ФИО: ");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String text = reader.readLine();
 
-        String familyName = text.substring(0, text.indexOf(' '));
-        String givenName = text.substring(text.indexOf(' ') + 1, text.lastIndexOf(' '));
-        String middleName = text.substring(text.lastIndexOf(' ') + 1);
+            if (text.length() > 64) {
+                System.out.println("ФИО не может превышать 64 символа");
+                continue;
+            }
 
-        System.out.println("Фамилия: " + familyName);
-        System.out.println("Имя: " + givenName);
-        System.out.println("Отчество: " + middleName);
+            String familyName = text.substring(0, text.indexOf(' '));
+            String givenName = text.substring(text.indexOf(' ') + 1, text.lastIndexOf(' '));
+            String middleName = text.substring(text.lastIndexOf(' ') + 1);
+
+            System.out.println("Фамилия: " + familyName);
+            System.out.println("Имя: " + givenName);
+            System.out.println("Отчество: " + middleName);
+            break;
+        }
     }
 }
