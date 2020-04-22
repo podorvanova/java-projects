9fe33a1af2c2325fefc625d742b2c6b3d09c4433
Maria Podorvanova <podorvanova.maria@gmail.com>
2020-04-22 17:16:57 +1000

    Replace manual verification with regex

--- 04_NumbersStringsAndDates/NameFormatting/src/Name.java
+++ 04_NumbersStringsAndDates/NameFormatting/src/Name.java
@@ -1,64 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Name {
-    public static int spaceCount(String text) {
-        int count = 0;
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == ' ') {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    /* Метод проверяет, что строка состоит из символов русского алфавита.
-       Кода символов от 'А' до 'я' последовательны в таблице символов Юникод.
-       Кода символов 'Ё' и 'ё' лежат вне промежутка от 'А' до 'я'.
-     */
-    public static boolean isTextRussian(String text) {
-        for (int i = 0; i < text.length(); i++) {
-            if ((text.charAt(i) < 'А' || text.charAt(i) > 'я') && text.charAt(i) != 'Ё' && text.charAt(i) != 'ё') {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public static void main(String[] args) throws IOException {
         while(true) {
             System.out.print("Введите ФИО: ");
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String text = reader.readLine();
+            final String text = reader.readLine();
 
             if (text.length() > 64) {
                 System.out.println("ФИО не может превышать 64 символа");
                 continue;
             }
 
-            int spaceCount = spaceCount(text);
-            if (spaceCount != 2) {
-                System.out.println("В ФИО должно быть 2 пробела");
+            final String regex = "([А-Я][а-я]*) ([А-Я][а-я]*) ([А-Я][а-я]*)";
+            final Pattern pattern = Pattern.compile(regex);
+            final Matcher matcher = pattern.matcher(text);
+
+            if (!matcher.find()) {
+                System.out.println("Фамилия, имя и отчество введены в неверном формате.\n" +
+                        "ФИО должно состоять из трех слов, начинающихся с заглавной буквы.");
                 continue;
             }
-
-            String[] words = text.split("\\s+");
-            String familyName = words[0];
-            String givenName = words[1];
-            String middleName = words[2];
-
-            if (Character.isLowerCase(familyName.charAt(0)) ||
-                    Character.isLowerCase(givenName.charAt(0)) ||
-                    Character.isLowerCase(middleName.charAt(0))) {
-                System.out.println("Фамилия, имя и отчество должны начинаться с заглавной буквы");
-                continue;
-            }
-
-            if (!isTextRussian(familyName) || !isTextRussian(givenName) || !isTextRussian(middleName)) {
-                System.out.println("ФИО должно содержать только символы русского алфавита");
-                continue;
-            }
+            String familyName = matcher.group(1);
+            String givenName = matcher.group(2);
+            String middleName = matcher.group(3);
 
             System.out.println("Фамилия: " + familyName);
             System.out.println("Имя: " + givenName);
