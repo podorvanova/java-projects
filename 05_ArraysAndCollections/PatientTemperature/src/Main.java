6d3b4d813173ce81744bfa259281c6bd58086257
Maria Podorvanova <podorvanova.maria@gmail.com>
2020-04-28 16:11:27 +1000

    Count number of healthy patients

--- 05_ArraysAndCollections/PatientTemperature/src/Main.java
+++ 05_ArraysAndCollections/PatientTemperature/src/Main.java
@@ -14,6 +14,8 @@ public class Main {
         System.out.println(Arrays.toString(patientsTemperature));
 
         System.out.println("Средняя температура: " + df.format(averageTemperature(patientsTemperature)));
+
+        System.out.println("Количество здоровых пациентов: " + numOfHealthyPatients(patientsTemperature));
     }
 
     public static double averageTemperature(double[] arrayOfTemperature) {
@@ -22,5 +24,15 @@ public class Main {
             sumTemperature += patient;
         }
         return sumTemperature / arrayOfTemperature.length;
+    }
+
+    public static int numOfHealthyPatients(double[] arrayOfTemperature) {
+        int count = 0;
+        for (double patient : arrayOfTemperature) {
+            if (patient >= 36.2 && patient <= 36.9) {
+                count++;
+            }
+        }
+        return count;
     }
 }
