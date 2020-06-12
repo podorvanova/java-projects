e4502d9158339c41d38b6204f542fe51b29589a9
Maria Podorvanova <podorvanova.maria@gmail.com>
2020-06-12 22:00:35 +1000

    Add second nameToNumbers map

--- 05_ArraysAndCollections/Contacts/src/Main.java
+++ 05_ArraysAndCollections/Contacts/src/Main.java
@@ -3,7 +3,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    private static final HashMap<String, String> contactsList = new HashMap<>();
+    private static final HashMap<String, String> numberToName = new HashMap<>();
+    private static final TreeMap<String, List<String>> nameToNumbers = new TreeMap<>();
     private static final Pattern patternName = Pattern.compile("^[А-Я][а-я]*$");
     private static final Pattern patternTelephone = Pattern.compile("^((\\+7)|8)(\\s|-|\\()?[0-9]{3}(\\s|-|\\))?([0-9](\\s|-)?){7}$");
 
@@ -21,32 +22,51 @@ public class Main {
                 getContactsList();
             } else if (matcherName.find()) {
                 String name = input;
-                System.out.println("Введите телефон, чтобы добавить контакт.");
-                while (true) {
-                    String secondInput = scanner.nextLine();
-                    matcherTelephone = patternTelephone.matcher(secondInput);
-                    if (matcherTelephone.find()) {
-                        String telephone = cleanTelephone(secondInput);
-                        contactsList.put(telephone, name);
-                        System.out.println("Контакт добавлен в телефонную книгу.");
-                        break;
-                    } else {
-                        System.out.println("Неверный формат телефона. Номер должен начинать с '8' или '+7' и может содержать пробелы, тире и скобочки.");
+                if (nameToNumbers.containsKey(name)) {
+                    System.out.println("Контакт уже содержится в телефонной книге.");
+                    System.out.println(name + ' ' + nameToNumbers.get(name));
+                } else {
+                    System.out.println("Введите телефон, чтобы добавить контакт.");
+                    while (true) {
+                        String secondInput = scanner.nextLine();
+                        matcherTelephone = patternTelephone.matcher(secondInput);
+                        if (matcherTelephone.find()) {
+                            String telephone = cleanTelephone(secondInput);
+                            numberToName.put(telephone, name);
+                            if (nameToNumbers.get(name) == null) {
+                                nameToNumbers.put(name, new ArrayList<>());
+                            }
+                            nameToNumbers.get(name).add(telephone);
+                            System.out.println("Контакт добавлен в телефонную книгу.");
+                            break;
+                        } else {
+                            System.out.println("Неверный формат телефона. Номер должен начинать с '8' или '+7' и может содержать пробелы, тире и скобочки.");
+                        }
                     }
                 }
             } else if (matcherTelephone.find()) {
                 String telephone = cleanTelephone(input);
-                System.out.println("Введите имя, чтобы добавить контакт.");
-                while (true) {
-                    String secondInput = scanner.nextLine();
-                    matcherName = patternName.matcher(secondInput);
-                    if (matcherName.find()) {
-                        String name = secondInput;
-                        contactsList.put(telephone, name);
-                        System.out.println("Контакт добавлен в телефонную книгу.");
-                        break;
-                    } else {
-                        System.out.println("Неверный формат имени. Имя должно начинаться с заглавной буквы и может состоять только из кириллических символов.");
+                if (numberToName.containsKey(telephone)) {
+                    System.out.println("Контакт уже содержится в телефонной книге.");
+                    System.out.println(numberToName.get(telephone) + ' ' + nameToNumbers.get(numberToName.get(telephone)));
+                    nameToNumbers.get(numberToName.get(telephone));
+                } else {
+                    System.out.println("Введите имя, чтобы добавить контакт.");
+                    while (true) {
+                        String secondInput = scanner.nextLine();
+                        matcherName = patternName.matcher(secondInput);
+                        if (matcherName.find()) {
+                            String name = secondInput;
+                            numberToName.put(telephone, name);
+                            if (nameToNumbers.get(name) == null) {
+                                nameToNumbers.put(name, new ArrayList<>());
+                            }
+                            nameToNumbers.get(name).add(telephone);
+                            System.out.println("Контакт добавлен в телефонную книгу.");
+                            break;
+                        } else {
+                            System.out.println("Неверный формат имени. Имя должно начинаться с заглавной буквы и может состоять только из кириллических символов.");
+                        }
                     }
                 }
             } else if (input.equals("END")) {
@@ -59,14 +79,12 @@ public class Main {
     }
 
     public static void getContactsList () {
-        if (contactsList.isEmpty()) {
+        if (nameToNumbers.isEmpty()) {
             System.out.println("Список контактов пуст. Введите номер телефона или имя для добавления.");
         } else {
             System.out.println("Список контактов:");
-            List<Map.Entry<String, String>> entries = new ArrayList<>(contactsList.entrySet());
-            entries.sort(Map.Entry.comparingByValue());
-            for (Map.Entry<String, String> pair : entries) {
-                System.out.println(pair.getValue() + ' ' + pair.getKey());
+            for (String key : nameToNumbers.keySet()) {
+                System.out.println(key + ' ' + nameToNumbers.get(key));
             }
         }
     }
