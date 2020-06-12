a7582811221bda7eec0c1d1cb5e5e560919fc555
Maria Podorvanova <podorvanova.maria@gmail.com>
2020-06-12 12:13:54 +1000

    Add phone number and name regex; add ability to create contacts

--- 05_ArraysAndCollections/Contacts/src/Main.java
+++ 05_ArraysAndCollections/Contacts/src/Main.java
@@ -1,7 +1,11 @@
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    private static final HashMap<Integer, String> contactsList = new HashMap<>();
+    private static final HashMap<String, String> contactsList = new HashMap<>();
+    private static final Pattern patternName = Pattern.compile("^[А-Я][а-я]*$");
+    private static final Pattern patternTelephone = Pattern.compile("^((\\+7)|8)(\\s|-|\\()?[0-9]{3}(\\s|-|\\))?([0-9](\\s|-)?){7}$");
 
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в телефонную книгу. Для просмотра контактов введите LIST. Для выхода из приложения введите END.");
@@ -10,8 +14,41 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
 
+            Matcher matcherName = patternName.matcher(input);
+            Matcher matcherTelephone = patternTelephone.matcher(input);
+
             if (input.equals("LIST")) {
                 getContactsList();
+            } else if (matcherName.find()) {
+                String name = input;
+                System.out.println("Введите телефон, чтобы добавить контакт.");
+                while (true) {
+                    String secondInput = scanner.nextLine();
+                    matcherTelephone = patternTelephone.matcher(secondInput);
+                    if (matcherTelephone.find()) {
+                        String telephone = cleanTelephone(secondInput);
+                        contactsList.put(telephone, name);
+                        System.out.println("Контакт добавлен в телефонную книгу.");
+                        break;
+                    } else {
+                        System.out.println("Неверный формат телефона. Номер должен начинать с '8' или '+7' и может содержать пробелы, тире и скобочки.");
+                    }
+                }
+            } else if (matcherTelephone.find()) {
+                String telephone = cleanTelephone(input);
+                System.out.println("Введите имя, чтобы добавить контакт.");
+                while (true) {
+                    String secondInput = scanner.nextLine();
+                    matcherName = patternName.matcher(secondInput);
+                    if (matcherName.find()) {
+                        String name = secondInput;
+                        contactsList.put(telephone, name);
+                        System.out.println("Контакт добавлен в телефонную книгу.");
+                        break;
+                    } else {
+                        System.out.println("Неверный формат имени. Имя должно начинаться с заглавной буквы и может состоять только из кириллических символов.");
+                    }
+                }
             } else if (input.equals("END")) {
                 scanner.close();
                 break;
@@ -26,11 +63,19 @@ public class Main {
             System.out.println("Список контактов пуст. Введите номер телефона или имя для добавления.");
         } else {
             System.out.println("Список контактов:");
-            List<Map.Entry<Integer, String>> entries = new ArrayList<>(contactsList.entrySet());
+            List<Map.Entry<String, String>> entries = new ArrayList<>(contactsList.entrySet());
             entries.sort(Map.Entry.comparingByValue());
-            for (Map.Entry<Integer, String> pair : entries) {
+            for (Map.Entry<String, String> pair : entries) {
                 System.out.println(pair.getValue() + ' ' + pair.getKey());
             }
         }
+    }
+
+    public static String cleanTelephone (String telephone) {
+        String cleanTelephone = telephone.replaceAll("[^+0-9]", "");
+        if (!telephone.startsWith("+7")) {
+            cleanTelephone = "+7" + cleanTelephone.substring(1);
+        }
+        return cleanTelephone;
     }
 }
