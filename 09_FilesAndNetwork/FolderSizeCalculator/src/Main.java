096f35036d729683b393090448224afbf66831df
Maria Podorvanova <podorvanova.maria@gmail.com>
2020-10-09 16:29:06 +1100

    Address reviewer's comments

--- 09_FilesAndNetwork/FolderSizeCalculator/src/Main.java
+++ 09_FilesAndNetwork/FolderSizeCalculator/src/Main.java
@@ -1,41 +1,23 @@
 import java.io.File;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
     private final static String[] SIZE_ABBREVIATION = {"б", "Кб", "Мб", "Гб", "Тб"};
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            try {
-                System.out.println("Введите путь до папки. Для выхода из приложения введите END.");
-                String input = scanner.nextLine();
-                if (input.equals("END")) {
-                    scanner.close();
-                    break;
-                }
-                if (!new File(input).exists()) {
-                    throw new IllegalArgumentException("Неправильный формат пути до папки.");
-                }
-                File file = new File(input);
-                System.out.println("Размер папки " + input + " составляет " + getHumanReadableSize(getFolderSize(file)) + ".");
+        try {
+            String input = args[0];
+            if (!new File(input).exists()) {
+                throw new IllegalArgumentException("Неправильный формат пути до папки.");
             }
-            catch (Exception ex) {
-                ex.printStackTrace();
-            }
+            Path path = Paths.get(input);
+            long filesSize = Files.walk(path).mapToLong(f -> f.toFile().length()).sum();
+            System.out.println("Размер папки " + input + " составляет " + getHumanReadableSize(filesSize) + ".");
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-    }
-
-    public static long getFolderSize(File folder) {
-        if (folder.isFile()) {
-            return folder.length();
-        }
-        long sum = 0;
-        File[] files = folder.listFiles();
-        for (File file : files) {
-            sum += getFolderSize(file);
-        }
-        return sum;
     }
 
     public static String getHumanReadableSize (long size) {
